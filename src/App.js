37201a83f151aec2f42fbd7f68dc7f8a6f870327
Big-beatz <kalmaerik@gmail.com>
2022-05-30 23:15:11 +0200

    data ophalen voor 1 pokemon, grotendeels compleet, abilities werkt nog niet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from "axios";
 
 function App() {
+    const [pokemonName, setPokemonName] = useState('')
+    const [sprite, setSprite] = useState('')
+    const [abilities, setAbilities] = useState([])
+    const [weight, setWeight] = useState('')
+    const [moves, setMoves] = useState('')
+
+    abilities.map(() => {})
+
+    async function fetchData(){
+        try{
+            const {data: pokemonData} = await axios.get(`https://pokeapi.co/api/v2/pokemon/bulbasaur`)
+            console.log(pokemonData)
+            setPokemonName(pokemonData.forms[0].name)
+            setSprite(pokemonData.sprites.front_default)
+            setAbilities(pokemonData.abilities)
+            setWeight(pokemonData.weight)
+            setMoves(pokemonData.moves.length)
+        }
+        catch(e){
+            console.error(e)
+        }
+    }
+    fetchData()
+
   return (
+      <>
+        <header>
+        <h1>Pokemon!</h1>
+        </header>
     <div>
-      Begin hier met de opdracht!
+      <article>
+          <h2>{pokemonName}</h2>
+        <img src={sprite} alt="pokemon"/>
+        <p>Moves: {moves}</p>
+        <p>Weight: {weight}</p>
+        <p>Abilities
+        </p>
+      </article>
     </div>
+      </>
   );
 }
 
